174acc345e9a121fe5c87d882f6cc09f1fd7f307
HADDAD Zineddine <hz_haddad@esi.dz>
2022-12-02 12:22:20 +0100

    wi

---
+++ lib/common_widgets/splash_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:untitled/constants/colors.dart';
+import 'package:untitled/features/marker_tracking/presentation/screens/home_screen.dart';
+
+
+final SplachScreen = AnimatedSplashScreen(
+  duration: 3000,
+  splash: Image.asset("assets/images/logo.png"),
+  nextScreen: const HomeScreen(
+    title: 'home_screen',
+  ),
+  splashTransition: SplashTransition.rotationTransition,
+  backgroundColor: primaryColor,
+);
